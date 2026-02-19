6607384bb6ee7277e118b77f118f01c2e51e48ab
Gebri Zsolt Pál <pgebrizsolt@gmail.com>
2026-02-19 12:19:56 +0100

    Add setup.py for digital audio library

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='digital-audio-worldstadium',
+    version='0.1.0',
+    description='A comprehensive library for digital audio manipulation and analysis.',
+    author='Pali766',
+    author_email='your.email@example.com',
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
